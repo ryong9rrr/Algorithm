00ca9bcd397603856ed01be719a536c486ce72b0
Yong SangYoon <64957267+ryong9rrr@users.noreply.github.com>
2021-02-10 22:27:52 +0900

    기본수학1~달팽이

--- 연습장.py
+++ 연습장.py
@@ -1,13 +1,36 @@
 import sys
+input = sys.stdin.readline
 
-a = sys.stdin.readline().split()
+A, B, V = map(int, input().split())
 
-b = list(input())
+day = 0
+loc = 0
 
-c = list(sys.stdin.readline())
+while True :
+    day += 1
+    loc += A
+    if loc < V :
+        loc -= B
+    elif loc >= V :
+        break
 
+print(day)
 
+#달팽이.. 시간초과
 
-print(a)
-print(b)
-print(c)
+import sys
+input = sys.stdin.readline
+
+A, B, V = map(int, input().split())
+
+day = 1
+loc = A
+
+while loc < V :
+    loc -= B
+    day += 1
+    loc += A
+
+print(day)
+
+# 이것도 시간초과
